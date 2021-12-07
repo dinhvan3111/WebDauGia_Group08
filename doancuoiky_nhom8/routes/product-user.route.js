6050149afaa360df6556c3c4262fc6c41248eb84
dinhvan3111 <85469453+dinhvan3111@users.noreply.github.com>
2021-12-07 15:35:49 +0700

    Thêm Product detail view
    Thêm giao diện chi tiết sản phẩm

--- doancuoiky_nhom8/routes/product-user.route.js
+++ doancuoiky_nhom8/routes/product-user.route.js
@@ -5,4 +5,9 @@ const router = express.Router();
 router.get('/', async function (req, res){
     res.render('vwProduct/products');
 });
+
+router.get('/detail', async function (req, res){
+    res.render('vwProduct/product_detail',{
+        layout: 'non_sidebar.hbs'});
+});
 export default router;
