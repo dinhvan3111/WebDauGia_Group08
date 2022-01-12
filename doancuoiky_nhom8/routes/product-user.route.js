4c87d168b303fdc327ffbe41109ba104ae264bb3
dinhvan3111 <85469453+dinhvan3111@users.noreply.github.com>
2022-01-13 00:09:47 +0700

    Sửa quyền xem thông tin của Seller và Bidder trang chi tiết sản phẩm
    - Người bán thấy username & link chi tiết đánh giá CÁC NGƯỜI ĐẤU GIÁ
    - Người đấu giá thấy được username & link chi tiết đánh giá NGƯỜI BÁN
    - Hiển thị thêm người giữ giữ giá / người chiến thắng + điểm đánh giá tại view chi tiết sản phẩm

--- doancuoiky_nhom8/routes/product-user.route.js
+++ doancuoiky_nhom8/routes/product-user.route.js
@@ -120,10 +120,12 @@ router.get('/:id', async function (req, res, next) {
     info.seller.starPercentage = starPercentage;
     info.seller.starPercentageRounded = starPercentageRounded;
     var priceHolder = await productModel.findPriceHolder(id_product);
-    if(priceHolder != null)
-        var priceHolderName = priceHolder.mask_name;
-    else
-        var priceHolderName = null;
+    priceHolder.upVoteRatio = await accountModel.getUpVoteRatio(priceHolder.id);
+    if(priceHolder.upVoteRatio.ratio){
+        priceHolder.isRating = true;
+    }
+    priceHolder.upVoteRatio = accountModel.starPercentage(priceHolder.upVoteRatio.ratio);
+    console.log(priceHolder.isRating);
     var bidder_win_now = null;
     var suggestPrice = +product_info.price;
     if (info.bidHistory !== null){
@@ -171,7 +173,7 @@ router.get('/:id', async function (req, res, next) {
         startDate: info.startDate,
         notEnoughVotes: info.notEnoughVotes,
         isWinner,
-        priceHolderName,
+        priceHolder,
         relativeProducts
     });
 });
